0dc03b5d683f4385e0d66cd4ec079c113a472497
PengL <397716215@qq.com>
2020-07-02 15:12:00 +0800

    91_Stream之消息持久化

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pengl
+ * @create 2020-07-02 下午 2:50
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamMQMain8803.class, args);
+	}
+
+}
